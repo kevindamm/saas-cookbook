3ee38fc5aeb3a4f6af473a6145b964e177aa9aaf
Kevin Damm <github@kevindamm.com>
2025-02-12 23:54:35 -0500

    add Naive-UI components to the book's presentation

--- book/.vitepress/theme/index.ts
+++ book/.vitepress/theme/index.ts
@@ -1,17 +1,60 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+
+import { setup } from '@css-render/vue3-ssr'
+import { NConfigProvider } from 'naive-ui'
+import { useRoute } from 'vitepress'
+import { defineComponent, h, inject } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+const { Layout } = DefaultTheme
+
+const CssRenderStyle = defineComponent({
+  setup() {
+    const collect = inject('css-render-collect') as any
+    return {
+      style: collect()
+    }
+  },
+
+  render() {
+    return h('css-render-style', {
+      innerHTML: this.style
+    })
+  }
+})
+
+const VitepressPath = defineComponent({
+  setup() {
+    const route = useRoute()
+    return () => {
+      return h('vitepress-path', null, [route.path])
+    }
+  }
+})
+
+const NaiveUIProvider = defineComponent({
+  render() {
+    return h(
+      NConfigProvider,
+      { abstract: true, inlineThemeDisabled: true },
+      {
+        default: () => [
+          h(Layout, null, { default: this.$slots.default?.() }),
+          // if import.meta.env.SSR ?
+          [h(CssRenderStyle), h(VitepressPath)]
+        ]
+      }
+    )
+  }
+})
+
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  Layout: NaiveUIProvider,
+  enhanceApp({ app }) {
+    const { collect } = setup(app)
+    app.provide('css-render-collect', collect)
   }
 } satisfies Theme
